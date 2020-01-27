fb2c82f90ab0a3cbd7d507f8e4ce564a6c675ff7
Kanav bhagat <kanav16046@iiitd.ac.in>
2020-01-28 00:25:22 +0530

    Delete hello.py
    not imp file(a test file which was used in the building phase)

--- hello.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-api_key = "dc6zaTOxFJmzC"
-
-params = {'q': "query",'api_key': api_key,'limit': 3,'rating': 'pg'}
-r = requests.get("http://api.giphy.com/v1/gifs/search", params=params)
-data = r.json()['data']
-print(data)
